c68f4fac309d67631ea5be7181eb341eb8205024
pasindu <pasindujalath@gmail.com>
2024-11-03 08:49:15 +1300

    Assignment 6 - Add Store

--- week6.client/src/components/stores/StoreTable.jsx
+++ week6.client/src/components/stores/StoreTable.jsx
@@ -1,26 +1,36 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './StoreTable.css';
+import { AddStorePage } from '../addstore/AddStorePage';
 
 export class StoreTable extends Component {
-
-    static displayName = StoreTable.name;
-
     constructor(props) {
         super(props);
-        this.state = { stores: [], loading: true };
-        //this.addStores = this.addCustomer.bind(this);
+        this.state = {
+            stores: [],
+            loading: false,
+            showAddStore: false
+        };
     }
 
-    componentDidMount() {
+    handleAddStoreButtonClick = () => {
+        this.setState({ showAddStore: true });
+    };
+
+    async componentDidMount() {
         this.populateStoresData();
     }
 
+    async populateStoresData() {
+        const response = await fetch('stores');
+        const data = await response.json();
+        this.setState({ stores: data, loading: false });
+    }
 
-    static renderStoresTable(stores) {
+    renderStoresTable(stores) {
         return (
             <table className="table table-striped" aria-labelledby="tableLabel">
                 <thead>
-                    <tr >
+                    <tr>
                         <th className="header">Id</th>
                         <th className="header">Name</th>
                         <th></th>
@@ -28,63 +38,30 @@ export class StoreTable extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    {stores.map(store =>
+                    {stores.map(store => (
                         <tr key={store.id}>
-                            <td className="record" >{store.id}</td>
-                            <td className="record"  >{store.name}</td>
-                            <td ><button>Update Store</button></td>
+                            <td className="record">{store.id}</td>
+                            <td className="record">{store.name}</td>
+                            <td><button>Update Store</button></td>
                             <td><button>Delete Store</button></td>
                         </tr>
-                    )}
+                    ))}
                 </tbody>
             </table>
         );
     }
 
     render() {
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : StoreTable.renderStoresTable(this.state.stores);
+        const { stores, loading, showAddStore } = this.state;
 
         return (
             <div>
-                <input id="storeName" name="storeName"
-                    type="text"
-                    placeholder="Store Name"                                      
-                />
-                <button onClick={this.addStores}>Add Store</button>
-                <h1 id="tableLabel">Stores</h1>
-                {contents}
+                <button onClick={this.handleAddStoreButtonClick}>Add Store</button>
+                {showAddStore && <AddStorePage />}
+                {loading ? <p><em>Loading...</em></p> : this.renderStoresTable(stores)}
             </div>
         );
     }
-
-    async addStores() {
-
-        let sName = document.getElementById('storeName').value;
-
-        console.log(sName);
-       
-        const data = await fetch(
-            'stores', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                id: 0,
-                name: sName
-            })
-        }).then((data) => data.json());
-
-        console.log(data); 
-
-        this.populateStoresData();
-    }
-
-
-
-    async populateStoresData() {
-        const response = await fetch('stores');
-        const data = await response.json();
-        this.setState({ stores: data, loading: false });
-    }
 }
+
+export default StoreTable;
